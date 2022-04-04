5601b384f6e157b07c019675d04417895e2c4ba0
pavelerokhin <pavel.erokhin@rstor.io>
2022-04-04 18:19:52 +0200

    apis refactor

--- server/webapi.go
+++ server/webapi.go
@@ -1,29 +1,24 @@
 package server
 
 import (
-	"../store"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-)
 
+	"github.com/gorilla/mux"
+
+	"../store"
+)
 
 type Handlers struct {
 	logger *log.Logger
-	db *store.DB
+	db     *store.DB
 }
 
 const message = "start here"
 
-
-func (h *Handlers) AddUser(w http.ResponseWriter, r *http.Request) {
+func (h *Handlers) AddOrUpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(message))
@@ -32,11 +27,6 @@ func (h *Handlers) AddUser(w http.ResponseWriter, r *http.Request) {
 func (h *Handlers) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
-	if r.Method != http.MethodDelete {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
 	id := getIdFromVars(r)
 
 	w.WriteHeader(http.StatusOK)
@@ -44,13 +34,8 @@ func (h *Handlers) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(message + " " + id))
 }
 
-func (h *Handlers) GetAllUsers(w http.ResponseWriter, r *http.Request) {
+func (h *Handlers) GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(message))
@@ -58,12 +43,6 @@ func (h *Handlers) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handlers) GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
 
 	id := getIdFromVars(r)
 
@@ -81,11 +60,6 @@ func (h *Handlers) Heartbeat(w http.ResponseWriter, r *http.Request) {
 func (h *Handlers) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
 	id := getIdFromVars(r)
 
 	w.WriteHeader(http.StatusOK)
@@ -94,9 +68,9 @@ func (h *Handlers) UpdateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func NewHandlers(logger *log.Logger, db *store.DB) *Handlers {
-	return &Handlers {
+	return &Handlers{
 		logger: logger,
-		db: db,
+		db:     db,
 	}
 }
 
@@ -107,12 +81,10 @@ func getIdFromVars(r *http.Request) string {
 
 func (h *Handlers) SetupRouts() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/users", h.GetAllUsers).Methods(http.MethodGet)
+	router.HandleFunc("/users/{filter}", h.GetUsers).Methods(http.MethodGet)
+	router.HandleFunc("/user/", h.AddOrUpdateUser).Methods(http.MethodPost)
 	router.HandleFunc("/user/{id}", h.GetUser).Methods(http.MethodGet)
-	router.HandleFunc("/users/filter", h.GetAllUsers).Methods(http.MethodGet)
-	router.HandleFunc("/user/add", h.AddUser).Methods(http.MethodPost)
-	router.HandleFunc("/delete/{id}", h.DeleteUser).Methods(http.MethodDelete)
-	router.HandleFunc("/update/{id}", h.UpdateUser).Methods(http.MethodPost)
+	router.HandleFunc("/user/{id}", h.DeleteUser).Methods(http.MethodDelete)
 
 	router.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)

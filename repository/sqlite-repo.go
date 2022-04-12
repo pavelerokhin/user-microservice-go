2dba4652687f17078c5b80c2e2f199c70d4c8f70
pavelerokhin <pavel.erokhin@rstor.io>
2022-04-12 23:50:00 +0200

    unhandled errors and refactoring, separating utils functions

--- repository/sqlite-repo.go
+++ repository/sqlite-repo.go
@@ -119,11 +119,3 @@ func (r *repo) Update(user *model.User) (*model.User, error) {
 
 	return user, err
 }
-
-// UTILS
-func paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		offset := (page - 1) * pageSize
-		return db.Offset(offset).Limit(pageSize)
-	}
-}

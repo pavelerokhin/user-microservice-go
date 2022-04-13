0daf68f76f212c8e577c5a5c5ff0ed9dd4e9922c
pavelerokhin <pavel.erokhin@rstor.io>
2022-04-13 12:06:10 +0200

    first commit

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-chi/chi v1.5.4
 	github.com/gorilla/mux v1.8.0
 	github.com/pavelerokhin/cleanarchitecture-restapi-go v0.0.0-20220411012928-73043a11f950
+	github.com/stretchr/testify v1.7.0 // indirect
 	go.mongodb.org/mongo-driver v1.9.0
 	gorm.io/driver/sqlite v1.3.1
 	gorm.io/gorm v1.23.4
---
+++ service/user-service_test.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var logger = log.New(os.Stdout, "testing-user-service", log.LstdFlags|log.Llongfile)
+
+func TestValidateEmptyUser(t *testing.T) {
+	testService := New(nil, logger)
+
+	err := testService.Validate(nil)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "the user object is empty")
+}
